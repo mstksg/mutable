4bde2138281148233c448e439cd745ce21e4a73b
Justin Le <justin@jle.im>
2020-01-19 16:30:16 -0800

    add access by position

--- src/Data/Mutable/MutPart.hs
+++ src/Data/Mutable/MutPart.hs
@@ -30,16 +30,21 @@ module Data.Mutable.MutPart (
   , updatePart, updatePart'
   -- * Built-in 'MutPart'
   , hkdMutPart
-  , FieldMut, fieldMut
+  , FieldMut, fieldMut, Fld(..)
+  , PosMut, posMut
   ) where
 
+import           Data.Coerce
 import           Data.Functor.Identity
 import           Data.Kind
 import           Data.Mutable.Class
 import           GHC.Generics
+import           GHC.OverloadedLabels
 import           GHC.TypeLits
 import qualified Data.GenericLens.Internal              as GL
 import qualified Data.Generics.Internal.Profunctor.Lens as GLP
+import qualified Data.Generics.Product.Fields           as GL
+import qualified Data.Generics.Product.Positions        as GL
 
 
 newtype MutPart m s a = MutPart { getMutPart :: Ref m s -> Ref m a }
@@ -83,19 +88,67 @@ hkdMutPart
     => z (MutPart m (z Identity))
 hkdMutPart = to $ hkdMutPart_ @m @z from
 
-class (Mutable m s, Mutable m a) => FieldMut fld m s a | fld s -> a where
-    fieldMut :: p fld -> MutPart m s a
+class (Mutable m s, Mutable m a) => FieldMut (fld :: Symbol) m s a | fld s -> a where
+    fieldMut :: Fld fld -> MutPart m s a
 
 instance
       ( Mutable m s
       , Mutable m a
       , Ref m s ~ GRef m s
       , GL.GLens' (HasTotalFieldPSym fld) (GRef_ m (Rep s)) (Ref m a)
-      , GL.GLens' (HasTotalFieldPSym fld) (Rep s) a
+      , GL.HasField' fld s a
       )
       => FieldMut fld m s a where
     fieldMut _ = MutPart $ GLP.view (GL.glens @(HasTotalFieldPSym fld)) . unGRef
 
-data HasTotalFieldPSym :: Symbol -> (GL.TyFun (Type -> Type) (Maybe Type))
+data HasTotalFieldPSym :: Symbol -> GL.TyFun (Type -> Type) (Maybe Type)
 type instance GL.Eval (HasTotalFieldPSym sym) tt = GL.HasTotalFieldP sym tt
 
+data Fld (fld :: Symbol) = Fld
+  deriving Show
+
+instance (s ~ s') => IsLabel s (Fld s') where
+    fromLabel = Fld
+
+class (Mutable m s, Mutable m a) => PosMut (i :: Nat) m s a | i s -> a where
+    posMut :: MutPart m s a
+
+instance
+      ( Mutable m s
+      , Mutable m a
+      , Ref m s ~ GRef m s
+      , gref ~ Fst (Traverse (GRef_ m (GL.CRep s)) 1)
+      , Coercible (GRef_ m (Rep s) ()) (gref ())
+      , GL.GLens' (HasTotalPositionPSym i) gref (Ref m a)
+      , GL.HasPosition' i s a
+      )
+      => PosMut i m s a where
+    posMut = MutPart $ GLP.view (GL.glens @(HasTotalPositionPSym i) @gref) . coerce @_ @(gref ()) . unGRef
+
+data HasTotalPositionPSym :: Nat -> GL.TyFun (Type -> Type) (Maybe Type)
+type instance GL.Eval (HasTotalPositionPSym t) tt = GL.HasTotalPositionP t tt
+
+type G = Type -> Type
+
+type family Traverse (a :: G) (n :: Nat) :: (G, Nat) where
+  Traverse (M1 mt m s) n
+    = Traverse1 (M1 mt m) (Traverse s n)
+  Traverse (l :+: r) n
+    = '(Fst (Traverse l n) :+: Fst (Traverse r n), n)
+  Traverse (l :*: r) n
+    = TraverseProd (:*:) (Traverse l n) r
+  Traverse (K1 _ p) n
+    = '(K1 (GL.Pos n) p, n + 1)
+  Traverse U1 n
+    = '(U1, n)
+
+type family Traverse1 (w :: G -> G) (z :: (G, Nat)) :: (G, Nat) where
+  Traverse1 w '(i, n) = '(w i, n)
+
+-- | For products, we first traverse the left-hand side, followed by the second
+-- using the counter returned by the left traversal.
+type family TraverseProd (c :: G -> G -> G) (a :: (G, Nat)) (r :: G) :: (G, Nat) where
+  TraverseProd w '(i, n) r = Traverse1 (w i) (Traverse r n)
+
+type family Fst (p :: (a, b)) :: a where
+  Fst '(a, b) = a
